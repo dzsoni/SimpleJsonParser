0e8373fb6235b620a15e6d2a2b70677ffeb0ce8e
dzsoni <fulekijan@gmail.com>
2022-11-13 11:42:59 +0100

    uncomment debug llines

--- src/SimpleJsonParser.cpp
+++ src/SimpleJsonParser.cpp
@@ -56,15 +56,12 @@ String SimpleJsonParser::getJSONValueByKeyFromString(String jsontext, String key
     searchPhrase.concat(key);
     searchPhrase.concat("\"");
     int fromPosition = jsontext.indexOf(searchPhrase, 0);
-
-    Serial.println(String("!") + String(fromPosition));
     if (fromPosition == -1)
     {
         // return because there is no status or it's null
         return String("");
     }
     fromPosition = fromPosition + key.length() + 2;
-    Serial.println(String("!!") + String(fromPosition) + String(jsontext.substring(fromPosition, fromPosition + 1)));
 
     fromPosition = _skipWhiteSpace(jsontext, fromPosition);
     if (jsontext.substring(fromPosition, fromPosition + 1) != ":")
