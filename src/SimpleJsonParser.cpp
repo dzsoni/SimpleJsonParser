04b0275fed606d7559b52de5d99d9f0cefd29ec4
dzsoni <fulekijan@gmail.com>
2024-07-08 14:10:10 +0200

    Update SimpleJsonParser.cpp

--- src/SimpleJsonParser.cpp
+++ src/SimpleJsonParser.cpp
@@ -8,6 +8,13 @@ SimpleJsonParser::~SimpleJsonParser()
 {
 }
 
+/**
+ * Retrieves the content of a file as a String.
+ *
+ * @param path The path to the file to read.
+ *
+ * @return The content of the file as a String.
+ */
 String SimpleJsonParser::fileToString(String path)
 {
     String content = "";
@@ -45,6 +52,14 @@ String SimpleJsonParser::fileToString(String path)
     return String("");
 }
 
+/**
+ * Retrieves the value associated with a given key from a JSON string.
+ *
+ * @param jsontext The JSON string to search.
+ * @param key The key to search for.
+ *
+ * @return The value associated with the given key, or an empty string if the key is not found.
+ */
 String SimpleJsonParser::getValueByKeyFromString(String jsontext, String key)
 {
     if (jsontext.length() == 0)
@@ -89,6 +104,16 @@ String SimpleJsonParser::getValueByKeyFromString(String jsontext, String key)
     return jsontext.substring(fromPosition);
 }
 
+/**
+ * Retrieves the value associated with the specified key from a JSON file located at the given path.
+ *
+ * @param path The file path of the JSON file.
+ * @param key The key to search for in the JSON file.
+ *
+ * @return The value corresponding to the provided key in the JSON file.
+ *
+ * @throws None
+ */
 String SimpleJsonParser::getValueByKeyFromFile(String path, String key)
 {
     String value = "";
@@ -188,6 +213,11 @@ String SimpleJsonParser::getValueByKeyFromFile(String path, String key)
     }
     return String("");
 }
+/**
+ * Skips any whitespace characters in the input file.
+ *
+ * @param f The input file to skip whitespace in.
+ */
 void SimpleJsonParser::_skipWhiteSpace(File f)
 {
     if (f)
@@ -201,6 +231,15 @@ void SimpleJsonParser::_skipWhiteSpace(File f)
     }
 }
 
+/**
+ * Skips whitespace characters in a given JSON text.
+ *
+ * @param jsontext The JSON text to skip whitespace in.
+ * @param frompos The starting position to skip whitespace from.
+ *
+ * @return The position after skipping whitespace.
+ *
+ */
 int SimpleJsonParser::_skipWhiteSpace(String jsontext, unsigned int frompos)
 {
     String nextch;
@@ -218,6 +257,14 @@ int SimpleJsonParser::_skipWhiteSpace(String jsontext, unsigned int frompos)
     }
     return frompos;
 }
+    /**
+     * Retrieves the number of entries in a JSON file located at the specified path.
+     *
+     * @param path The path to the JSON file.
+     *
+     * @return The number of entries in the JSON file, or -1 if the file does not exist or there is a serious problem with SPIFFS.
+     *
+     */
 int SimpleJsonParser::getNumberOfEntriesFromFile(String path)
 {
     int count = 0;
@@ -264,6 +311,14 @@ int SimpleJsonParser::getNumberOfEntriesFromFile(String path)
     return -1;
 }
 
+/**
+ * Counts the number of entries in a JSON string.
+ *
+ * @param jsontxt The JSON string to count the entries from.
+ *
+ * @return The number of entries in the JSON string.
+ *
+ */
 int SimpleJsonParser::getNumberOfEntriesFromString(String jsontxt)
 {
     int count = 0;
@@ -287,6 +342,15 @@ int SimpleJsonParser::getNumberOfEntriesFromString(String jsontxt)
     return count;
 }
 
+/**
+ * Retrieves the key at the specified index from a JSON file.
+ *
+ * @param path The path to the JSON file.
+ * @param index The index of the key to retrieve.
+ *
+ * @return The key at the specified index, or an empty string if the file does not exist or the index is out of range.
+ *
+ */
 String SimpleJsonParser::getKeybyIndexFromFile(String path, uint32_t index)
 {
     uint32_t count = 0;
@@ -363,6 +427,16 @@ String SimpleJsonParser::getKeybyIndexFromFile(String path, uint32_t index)
     }
     return String("");
 }
+/**
+ * Retrieves a value from a JSON file at a given index.
+ *
+ * @param path the path to the JSON file
+ * @param index the index of the value to retrieve
+ *
+ * @return the value at the specified index, or an empty string if the file
+ *         does not exist or the index is out of range.
+ *
+ */
 String SimpleJsonParser::getValuebyIndexFromFile(String path, uint32_t index)
 {
     uint32_t count = 0;
@@ -444,6 +518,15 @@ String SimpleJsonParser::getValuebyIndexFromFile(String path, uint32_t index)
 
     return String("");
 }
+/**
+ * Retrieves the key from the JSON string by the specified index.
+ *
+ * @param jsontxt The JSON string to extract the key from.
+ * @param index The index of the key to retrieve.
+ *
+ * @return The extracted key from the JSON string. If the index is out of range or the JSON is corrupt, an empty string is returned.
+ *
+ */
 String SimpleJsonParser::getKeybyIndexFromString(String jsontxt, int index)
 {
     int count = 0;
@@ -495,6 +578,15 @@ iloopend:
     return String("");
 }
 
+/**
+ * Retrieves the value from the JSON string by the specified index.
+ *
+ * @param jsontxt The JSON string to extract the value from.
+ * @param index The index of the value to retrieve.
+ *
+ * @return The extracted value from the JSON string.
+ *
+ */
 String SimpleJsonParser::getValuebyIndexFromString(String jsontxt, int index)
 {
     int count = 0;
@@ -542,6 +634,14 @@ String SimpleJsonParser::getValuebyIndexFromString(String jsontxt, int index)
     }
     return String("");
 }
+/**
+ * Extracts keys and values from a JSON file given the file path.
+ *
+ * @param path The path to the JSON file.
+ *
+ * @return A vector of pairs containing the extracted keys and values.
+ *
+ */
 std::vector<std::pair<String, String>> SimpleJsonParser::extractKeysandValuesFromFile(String path)
 {
     std::vector<std::pair<String, String>> result;
